191e0c01b49d039da83a6d4b9b0e55812352e067
darshah05 <128460991+darshah05@users.noreply.github.com>
2024-08-02 12:02:18 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Script from "next/sript";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,14 +14,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
    
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SCZ9P4PQ21"></script>
-<script>
+<Script async src="https://www.googletagmanager.com/gtag/js?id=G-SCZ9P4PQ21"></Script>
+<Script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-SCZ9P4PQ21');
-</script>
+</Script>
       <body className={`${inter.className} `}>
         <Navbar />
         <div className="h-[calc(100vh-70px)]">{children}</div>
