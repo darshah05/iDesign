c74dad4f2389ac07bc4a3340eee56d4a09125f88
darshah05 <128460991+darshah05@users.noreply.github.com>
2024-08-02 12:06:41 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Script from "next/script";
-
 const inter = Inter({ subsets: ["latin"] });
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
   title: "iDesign",
@@ -14,18 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
    
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-SCZ9P4PQ21"></Script>
-<Script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-SCZ9P4PQ21');
-</Script>
       <body className={`${inter.className} `}>
         <Navbar />
         <div className="h-[calc(100vh-70px)]">{children}</div>
       </body>
+  <GoogleAnalytics gaId="G-SCZ9P4PQ21" />
     </html>
   );
 }
